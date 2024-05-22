bcf5a0cbbe034f09c76319004db469a4b421e90f
Sujoy Batabyal <sujoy_batabyal@persistent.com>
2024-05-22 11:26:25 +0530

    update the AWS variables

---
+++ aws_vars/provider.tf
@@ -0,0 +1,7 @@
+provider "aws" {
+  region     = "ap-south-1"
+  access_key = "Your Key"
+  secret_key = "Your Key"
+
+}
+
---
+++ aws_vars/variables.tf
@@ -0,0 +1,12 @@
+variable "vpn_ip" {
+    default = "101.20.30.50/32"
+}
+variable "app_port" {
+    default = "8080"
+}
+variable "ssh_port" {
+    default = "22"
+}
+variable "ftp_port" {
+    default = "21"
+}
