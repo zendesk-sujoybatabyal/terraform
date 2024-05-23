dc1d53ea1354fcf245e535e55e9683934ae935d9
Sujoy Batabyal <sujoykr@gmail.com>
2024-05-23 14:04:19 +0530

    aws vpc multiple security group with list

--- AWS_resources/list-data-type.tf
+++ AWS_resources/list-data-type.tf
@@ -1,5 +1,6 @@
 resource "aws_instance" "web" {
     ami = "ami-0f58b397bc5c1f2e8"
     instance_type = "t2.micro"
+    count = 2
     vpc_security_group_ids = ["sg-083c3af9d6d752a91","sg-0748659397f60e673"]
 }
